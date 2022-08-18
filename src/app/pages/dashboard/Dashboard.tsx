96731e34a06c12c64131f6cbe49225de84c4e660
Isadora Riegert <isadora.riegert@gmail.com>
2022-08-18 18:01:52 +0100

    feat: add count with useRef

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,16 @@
+import { useRef } from "react"
 import { Link } from "react-router-dom"
 
 export const Dashboard = () => {
+    const countRef = useRef(0);
+
     return (
-        <><h1>Dashboard</h1>
-        <Link to={"/login"}>Login</Link></>
+        <>
+            <h1>Dashboard</h1>
+            <p>Counter (initial): {countRef.current}</p>
+            <button onClick={()=> countRef.current++}>Som</button>
+            <button onClick={() => console.log(countRef.current)}>Show final count</button>
+            <Link to={"/login"}>Login</Link>
+        </>
     )
 }
