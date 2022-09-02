19e03af84a244d1d80f4df431b0c3304b2c34a2c
Isadora Riegert <isadora.riegert@gmail.com>
2022-09-02 12:12:23 +0100

    feat: add useState

--- src/app/shared/context/LoggedUser.tsx
+++ src/app/shared/context/LoggedUser.tsx
@@ -1,25 +1,32 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { createContext } from "react";
 
 interface ILoggedUserContextData {
     userName: string;
     logout: () => void;
 }
-
-export const LoggedUserContext = createContext({} as ILoggedUserContextData);
-
 interface ILoggedUserProviderProps {
     children: React.ReactNode;
 }
 
+export const LoggedUserContext = createContext({} as ILoggedUserContextData);
+
 export const LoggedUserProvider = ({children}:ILoggedUserProviderProps) => {
+
+    const [ name, setName ] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setName('Lucas')
+        }, 600)
+    })
 
     const handleLogout = useCallback(() => {
         console.log("logout succeed")
     }, []);
 
     return (
-        <LoggedUserContext.Provider value={{userName: 'Isadora', logout: handleLogout}}>
+        <LoggedUserContext.Provider value={{userName: name, logout: handleLogout}}>
             {children}
         </LoggedUserContext.Provider>
     )
